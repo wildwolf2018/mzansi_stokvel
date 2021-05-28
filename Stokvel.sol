3e65c5b32021a806933c5cd319aaa20273aaa302
Thabo Thage <36538335+wildwolf2018@users.noreply.github.com>
2021-05-28 16:33:19 +0200

    Updated payment to member

--- Stokvel.sol
+++ Stokvel.sol
@@ -8,16 +8,13 @@ contract Stokvel {
     uint public totalMembers;
     uint public payeeIndex = 0;
     uint public creationDate;
-    uint public totalFunds;
     uint public donationDate;
     bool public hasDonated = false;
     uint public lastPayDate;
     uint public contractBalance;
-    unit public membershipFee = 2000000000000000000 wei;
+    uint public membershipFee = 2000000000000000000 wei;
     uint public paymentToMember;
-    
     address public chairperson;
-    
     
     struct Member{
         uint payDate;
@@ -90,25 +87,22 @@ contract Stokvel {
 	  members[beneficiary].isPaid = true;
 	  members[beneficiary].isInArrears = true;    
 
-	  uint payment = totalMembers * 1 ether;
-	  assert(payment < address(this).balance);
-	  totalFunds = address(this).balance - payment;
-	  beneficiary.transfer(payment);
+	  assert(paymentToMember < address(this).balance);
 	  lastPayDate = members[beneficiary].payDate;
 	  
-	  emit MemberPaid(beneficiary, payment);
+	  beneficiary.transfer(paymentToMember);
+	  contractBalance = address(this).balance;
+	  emit MemberPaid(beneficiary, paymentToMember);
 
 	  if(payeeIndex >= accounts.length - 1){
 	    reset();
 	  }
-        }
-     }
+       }
     }
+  }
     
   function reset() private{
-     if(payeeIndex >= accounts.length - 1){
-       payeeIndex = 0;
-       uint lasyPayDate = members[accounts[accounts.length-1]].payDate;
+     if(payeeIndex >= 0 && payeeIndex < accounts.length){
        for(uint i = 0; i < accounts.length; i++){
            members[accounts[i]].isPaid = false;
            members[accounts[i]].payDate = lastPayDate + (i+1) * 14 days;
@@ -117,15 +111,14 @@ contract Stokvel {
   }
   
   function nextPayee() private view returns(uint){
-      address prevPayee;
-      for(uint i = 0; i < accounts.length; i++){
-         address beneficiary = accounts[i];
-         if(block.timestamp > lastPayDate && block.timestamp <= members[beneficiary].payDate){
-            return i;   
-         }
-     }
-     return 0;
-  }
+    for(uint i = 0; i < accounts.length; i++){
+       address beneficiary = accounts[i];
+       if(block.timestamp > lastPayDate && block.timestamp <= members[beneficiary].payDate){
+          return i;   
+       }
+    }
+    return 0;
+ }
   
  function vote(address voter, uint _causeIndex) public{
    require(members[voter].payDate != 0, "Only members can vote");
@@ -148,30 +141,29 @@ contract Stokvel {
           uint maxVotesIndex = 0;
           uint maxVotes = causes[0].voteCount;
            
-          for(uint i = 0; i < causes.length; i++){
+          for(uint i = 1; i < causes.length; i++){
 	    if(causes[i].voteCount > maxVotes){
                 maxVotesIndex = i;
             }
           }
            
           assert(address(this).balance > donation);
-          totalFunds = address(this).balance + donation;
           causes[maxVotesIndex].account.transfer(donation);
+	  contractBalance = address(this).balance;
           emit DonationMade(causes[maxVotesIndex].name, causes[maxVotesIndex].account, donation);
       }
   }
   
   function hasFunds() private returns(bool) {
-      uint minBalance = totalMembers * 1 ether;
-      if(payeeIndex == 0 && (address(this).balance < totalMembers * minBalance)){
-          uint startTime = block.timestamp;
+     if(payeeIndex == 0 && (address(this).balance < totalMembers * paymentToMember)){
+        uint startTime = block.timestamp;
           
-          for(uint i = 0; i < accounts.length; i++){
-            members[accounts[i]].payDate = startTime + (i + 1) * 14 days;
-         }
-         return false;
-      }
-      return true;
+        for(uint i = 0; i < accounts.length; i++){
+           members[accounts[i]].payDate = startTime + (i + 1) * 14 days;
+        }
+        return false;
+     }
+     return true;
   }
   
   function getNumberOfCauses() public view returns(uint){
